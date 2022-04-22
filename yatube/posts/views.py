a3d97bdb81bcb7acf92a84240087e3dfa4023346
Ву Чунг <xeyu134@gmail.com>
2022-04-22 14:32:53 +0300

    Added templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Главная страница.
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 # Страница постов группы.
