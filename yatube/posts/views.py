ec838a906065f5a135bdeaaf5f03402115c3420b
Ву Чунг <xeyu134@gmail.com>
2022-04-24 14:03:25 +0300

    Added dicts into templates.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,10 +5,18 @@ from django.shortcuts import render
 # Главная страница.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
 
 # Страница постов группы.
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('posts.urls')),
-    path('group/<slug:pk>', include('posts.urls')),
+    path('group/<slug:slug>', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
