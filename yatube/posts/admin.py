61a4433c76bbde363e8315a6cd7f2cbf340b6a43
Ву Чунг <xeyu134@gmail.com>
2022-04-27 21:17:27 +0300

    Added new model Group, migrated it, created new groups.

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,20 +1,29 @@
 from django.contrib import admin
-from .models import Post
+from .models import Group, Post
 
 # Register your models here.
 
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('text', 'pub_date', 'author')
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
+    list_editable = ('group',)
     # Это свойство сработает для всех колонок: где пусто — там будет эта строка
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_field = ('title',)
+    list_filter = ('title',)
     empty_value_display = '-пусто-'
 
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -15,3 +15,19 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='posts'
+    )
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
