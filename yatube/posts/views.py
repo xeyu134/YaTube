d2d4994c8107241c33281c41e4d0841ef511a5db
Ву Чунг <xeyu134@gmail.com>
2022-04-28 14:11:28 +0300

    New template for group_list, fixed link redirection.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Импортируем модель, чтобы обратиться к ней
-from .models import Post
+from .models import Post, Group
 
 
 # Главная страница.
@@ -9,7 +9,8 @@ def index(request):
     title = 'Это главная страница проекта Yatube'
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    # отсортированных по полю pub_date по убыванию
+    # (от больших значений к меньшим).
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
@@ -19,10 +20,20 @@ def index(request):
 
 
 # Страница постов группы.
-def group_list(request):
+def group_list(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
